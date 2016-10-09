750cf4f50f1b1d5bf34f488143e84d9c6264509c
polun <965076377@qq.com>
2016-10-10 00:01:41 +0800

    update

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
+
+auth_url = 'https://api.weibo.com/oauth2/authorize?client_id=4177092657\
+    &response_type=code&redirect_uri=http://123.56.226.235:8090/weibo/redirectURI'
 
 @app.route('/')
 def main_page():
@@ -9,7 +12,12 @@ def main_page():
 
 @app.route('/weibo_favorite')
 def weibo_favorite():
+    return redirect(auth_url, code=302)
     return render_template('weibo_favorite.html')
+
+@app.route('/weibo/redirectURI')
+def weibo_redirectURI(code):
+    print code
 
 if __name__ == '__main__':
     app.run(debug=True, port=8090)
