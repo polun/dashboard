d3c9686cad09b6e644aa3f9660a29e34cce7be3c
zhenqiang <zhangzq@heptax.com>
2016-10-09 19:13:10 +0800

    update

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## 2016年10月8日19:05:13
 开始搭建项目了
+边学习[Flask](http://docs.jinkan.org/docs/flask/)边做这个项目
--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return '主页'
+    return render_template('index.html')
+
+@app.route('/weibo_favorite')
+def weibo_favorite():
+    return render_template('weibo_favorite.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8090)
